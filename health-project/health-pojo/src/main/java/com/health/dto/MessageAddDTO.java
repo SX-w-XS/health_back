efb6153fa7faeff4d2e32d35acc503a2a9c07ce2
SX-w-XS <3546446397@qq.com>
2025-03-23 15:31:48 +0800

    新闻完善

--- health-project/health-pojo/src/main/java/com/health/dto/MessageAddDTO.java
+++ health-project/health-pojo/src/main/java/com/health/dto/MessageAddDTO.java
@@ -1,5 +1,7 @@
 package com.health.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.Column;
 import java.util.Date;
 
@@ -14,25 +16,30 @@ public class MessageAddDTO {
     /**
      * 标题
      */
+    @ApiModelProperty("标题")
     private String title;
 
     /**
      * 跳转url
      */
+    @ApiModelProperty("跳转url")
     private String url;
 
     /**
      * 类型
      */
+    @ApiModelProperty("类型")
     private Integer kind;
 
 
     /**
      * 内容
      */
+    @ApiModelProperty("内容")
     private String content;
 
 
+    @ApiModelProperty("创建时间")
     private Date createtime;
 
 }
--- health-project/health-pojo/src/main/java/com/health/dto/MessageDTO.java
+++ health-project/health-pojo/src/main/java/com/health/dto/MessageDTO.java
@@ -1,6 +1,7 @@
 package com.health.dto;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -24,7 +25,7 @@ public class MessageDTO {
     /**
      * 标题
      */
-
+    @ApiModelProperty("标题")
     private String title;
 
 
@@ -32,7 +33,7 @@ public class MessageDTO {
     /**
      * 类型
      */
-
+    @ApiModelProperty("类型")
     private Integer kind;
 
 
@@ -40,6 +41,6 @@ public class MessageDTO {
     /**
      * 内容
      */
-
+    @ApiModelProperty("内容")
     private String content;
 }
